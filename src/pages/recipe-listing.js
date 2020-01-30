de3e557650481233957ba5dfbc7c7b2f1957cbff
Benji Fisher <benji@hook42.com>
2020-01-30 00:14:14 -0500

    Update because Umami switched from Image to Media fields
    Unfortunately, this commit contains some unrelated minor changes.

--- src/pages/recipe-listing.js
+++ src/pages/recipe-listing.js
@@ -1,46 +1,50 @@
 import React from "react"
-import {graphql} from "gatsby"
+import { graphql } from "gatsby"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import RecipeTeaser from "../components/recipe-teaser.js"
+import RecipeTeaser from "../components/recipe-teaser"
 
 const RecipeListingPage = ({data}) => (
   <Layout>
     <SEO title="Recipes" />
     <h1>Recipes</h1>
     { data.allNodeRecipe.edges.map((recipe) => (
-    <RecipeTeaser
+      <RecipeTeaser
         key={recipe.node.id}
         recipeDate={recipe.node.created}
         recipeTitle={recipe.node.title}
         recipeSummary={recipe.node.field_summary.value}
-        recipeImg={recipe.node.relationships.field_image.localFile.childImageSharp.fixed}
-    />
+        recipeImg={recipe.node.relationships.field_media_image.relationships.field_media_image.localFile.childImageSharp.fixed}
+      />
     ) )}
   </Layout>
 )
 
 export const query = graphql`
- query RecipeQuery {
+  query RecipeQuery {
     allNodeRecipe {
       edges {
         node {
           id
           title
-          created(formatString: "MMMM, Do, YYYY")
+          created(formatString: "MMMM Do, YYYY")
           field_summary {
             value
             format
             processed
           }
           relationships {
-            field_image {
-              localFile {
-                childImageSharp {
-                  fixed(width: 125, height: 125) {
-                    ...GatsbyImageSharpFixed
+            field_media_image {
+              relationships {
+                field_media_image {
+                  localFile {
+                    childImageSharp {
+                      fixed(width: 125, height: 125) {
+                        ...GatsbyImageSharpFixed
+                      }
+                    }
                   }
                 }
               }
@@ -51,5 +55,4 @@ export const query = graphql`
     }
   }
 `
-
 export default RecipeListingPage
